6102d192463e4f2ff6cd7aefb1d238497ec4bc12
ngvh2605 <ngvh2605@gmail.com>
2022-03-29 16:22:40 +1100

    Add GET/POST functions

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,16 +5,66 @@ import {
   IonInput,
   IonItem,
   IonLabel,
-  IonList,
   IonPage,
   IonTitle,
   IonToolbar,
+  useIonToast,
 } from "@ionic/react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./Home.css";
 
 const Home: React.FC = () => {
   const [input, setInput] = useState("");
+  const [presentToast] = useIonToast();
+  const axios = require("axios");
+
+  useEffect(() => {
+    axios({
+      method: "get",
+      url: "https://3ic8ifp6ye.execute-api.ap-southeast-2.amazonaws.com/prod/userData",
+      headers: {
+        "x-api-key": "5aa698ecbfa345838559ec6133fb9529",
+      },
+    })
+      .catch((err: any) => {
+        console.log("get err: ", err);
+      })
+      .then((response: any) => {
+        console.log("get response: ", response);
+        if (response && response.data && response.data.url) {
+          setInput(response.data.url);
+        }
+      });
+  }, []);
+
+  const handlePost = () => {
+    axios({
+      method: "post",
+      url: "https://3ic8ifp6ye.execute-api.ap-southeast-2.amazonaws.com/prod/userData",
+      headers: {
+        "x-api-key": "5aa698ecbfa345838559ec6133fb9529",
+      },
+      data: {
+        url: input,
+      },
+    })
+      .catch((err: any) => {
+        console.log("post err: ", err);
+        presentToast({
+          message: "POST failed, please try again!",
+          color: "danger",
+          duration: 3000,
+        });
+      })
+      .then((response: any) => {
+        console.log("post response: ", response);
+        presentToast({
+          message: "POST successfully!",
+          color: "success",
+          duration: 3000,
+        });
+      });
+  };
 
   return (
     <IonPage>
@@ -35,9 +85,10 @@ const Home: React.FC = () => {
         </IonItem>
         <br />
         <IonButton
+          disabled={!input}
           expand="block"
           onClick={() => {
-            console.log(input);
+            handlePost();
           }}
         >
           Save URL
